793cb89da06374d362b232cf5caea38090664b2f
kartik.yadav <kartik.yadav@udaan.com>
2023-10-20 23:11:24 +0530

    finicky config added

---
+++ .dotfiles/finicky/.finicky.js
@@ -0,0 +1,10 @@
+module.exports = {
+  defaultBrowser: "Safari",      
+  handlers: [
+    {
+      match: /^https?:\/\/notion$/,
+      browser: "Safari",
+      // profile: "Profile 5"
+    }
+  ]
+}
